16ecc2b9daf2b64b28e4c2f1c17ed8560f6c91b5
cellouRetailShake <cellu@retailshake.com>
2025-07-31 18:39:38 +0200

    💡 refactor: Initialize environment variables and update seed data insertion in the database script

--- src/db/scripts/seed.exercise.ts
+++ src/db/scripts/seed.exercise.ts
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
 import pg from 'pg'
+import initDotEnv from './env'
 
-// 🐶 importe 'initDotEnv' la fonction d'initialisation des varialbes d'environement
-//import initDotEnv from './env'
-
-// 🐶 Execute la fonction
-//initDotEnv()
+initDotEnv()
 
 const seed = async () => {
   if (!process.env.POSTGRES_URL) {
@@ -24,7 +21,17 @@ const seed = async () => {
   const start = Date.now()
 
   // ⛏️ Suprime cette requête et remplace la par une requête d'insertion des données. (db/scripts/sql/seed.sql)
-  await client.query(` SELECT 1`)
+  await client.query(`INSERT INTO Todo (title, isCompleted, createdAt, updatedAt) VALUES
+('Learn TypeScript', false, '2024-01-01 10:00:00', '2024-01-01 10:00:00'),
+('Build a Next.js App', false, '2024-01-02 11:00:00', '2024-01-02 11:00:00'),
+('Write a Blog Post', true, '2024-01-03 09:00:00', '2024-01-03 09:00:00'),
+('Create a YouTube Video', false, '2024-01-04 14:00:00', '2024-01-04 14:00:00'),
+('Read a Technical Book', true, '2024-01-05 08:00:00', '2024-01-05 08:00:00'),
+('Update Portfolio', false, '2024-01-06 15:00:00', '2024-01-06 15:00:00'),
+('Attend a Webinar', true, '2024-01-07 12:00:00', '2024-01-07 12:00:00'),
+('Practice Coding Challenges', false, '2024-01-08 17:00:00', '2024-01-08 17:00:00'),
+('Network with Peers', false, '2024-01-09 13:00:00', '2024-01-09 13:00:00'),
+('Plan Next Month’s Goals', true, '2024-01-10 18:00:00', '2024-01-10 18:00:00');`)
 
   const end = Date.now()
 
